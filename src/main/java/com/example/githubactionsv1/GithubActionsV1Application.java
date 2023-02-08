c87ef1ea087c41042f554c7149ba78581dd923fb
Daniel Alexander Dam <dani36i9@stud.kea.dk>
2023-02-08 10:22:23 +0100

    first commit

---
+++ src/main/java/com/example/githubactionsv1/GithubActionsV1Application.java
@@ -0,0 +1,13 @@
+package com.example.githubactionsv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionsV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubactionsv1/GithubActionsV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubactionsv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
